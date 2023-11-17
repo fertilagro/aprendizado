eea24ca256d25b27755a6ce3d911245bdbd74dcd
fertilagro <fertilaagro@gmail.com>
2023-11-16 22:48:21 -0300

    Cidade

--- src/app/fertilagro/ger/ger.module.ts
+++ src/app/fertilagro/ger/ger.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { GerRoutingModule } from './ger-routing.module';
 import { PessoaModule } from './pessoa/pessoa.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CidadeModule } from './cidade/cidade.module';
 
 @NgModule({
   declarations: [],
   imports: [
     GerRoutingModule,
     PessoaModule,
-    SharedModule
+    SharedModule,
+    CidadeModule
   ],
   exports: [
     PessoaModule
